ef338e606b2bfcaef4481ff443578f2c32df777a
Issam <issam-laazayri@hotmail.com>
2024-04-29 17:19:51 +0200

    deploy app

---
+++ build/precache-manifest.8fe9576e79dccbeb157d216fd40b90b6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01d1cf149016243b50c5d4778eb4b364",
+    "url": "/app-calcul.git/index.html"
+  },
+  {
+    "revision": "20049d26d7cfb6da13c1",
+    "url": "/app-calcul.git/static/css/main.1a52d56b.chunk.css"
+  },
+  {
+    "revision": "0eacf60fadf230c86fe3",
+    "url": "/app-calcul.git/static/js/2.bf86286d.chunk.js"
+  },
+  {
+    "revision": "783f14fa45b10e088e68f98251448010",
+    "url": "/app-calcul.git/static/js/2.bf86286d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e64aefffb74ed727eb8f",
+    "url": "/app-calcul.git/static/js/3.11ebb954.chunk.js"
+  },
+  {
+    "revision": "6ba29c55aa84150688ff04c7d480564f",
+    "url": "/app-calcul.git/static/js/3.11ebb954.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c118d2ab491010c75f7",
+    "url": "/app-calcul.git/static/js/4.cddfc277.chunk.js"
+  },
+  {
+    "revision": "b0bbedcb86a939e7e485f3fcd045c3bf",
+    "url": "/app-calcul.git/static/js/4.cddfc277.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20049d26d7cfb6da13c1",
+    "url": "/app-calcul.git/static/js/main.326f575f.chunk.js"
+  },
+  {
+    "revision": "17b012d99af96a8d5f81",
+    "url": "/app-calcul.git/static/js/runtime-main.7de6068f.js"
+  }
+]);
